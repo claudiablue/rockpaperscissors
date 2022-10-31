f6e25463aeeb77d3fa3d3d38bb8545b8c0d088f8
claudia <lee.ca96@me.com>
2022-10-30 21:13:15 -0400

    Add a few more functions and variables, game not complete

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 const choices = ['rock', 'paper', 'scissors'];
+
 function getComputerChoice() {
     choices[Math.floor(Math.random()*choices.length)]
 }
 
-const playerSelection = pepper
+const playerSelection = prompt("Rock, Paper, or Scissors?").toLowerCase();
 const computerSelection = getComputerChoice();
+
+function playRound(playerSelection, computerSelection){
+}
+
+console.log(playRound(playerSelection, computerSelection));
